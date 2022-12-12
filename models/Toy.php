d233348f03ab9da0183e05f6508348f7014ef560
MatTheoo89 <matteobellomo89@gmail.com>
2022-12-12 13:26:28 +0100

    first commit

---
+++ models/Toy.php
@@ -0,0 +1,13 @@
+<?php
+
+class Toy extends Product{
+
+    public $feature;
+    public $size;
+
+    public function __construct($_feature, $_size)
+    {
+        $this->feature = $_feature;
+        $this->size = $_size;
+    }
+}
